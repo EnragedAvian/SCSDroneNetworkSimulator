52757cfcc651e3654cb2d5879b160b56524c9149
CameronBossalini <cameron@bossalini.net>
2017-10-10 12:14:55 -0500

    Changed variable names

--- Neighbor.java
+++ Neighbor.java
@@ -1,14 +1,24 @@
 // Neighbor class contains a links between the graph of trajectories
 
 public class Neighbor {
-	int hubTrajID;
-	int neighborTrajID;
-	float angle;	// Angle between the two trajectories measured from the positive x axis of the hub trajectory
-	float hubDetectIn;
-	float neighborDetectIn;
-	float hubTransitionIn;
-	float hubTransitionOut;
-	float neighborTransitionIn;
-	float neighborTransitionOut;
+	int trajID_a;	// IDs of the specific trajectories
+	int trajID_b;
+	float angle_a;	// Angle between positive x axis and the angle of the trajectory
+	float angle_b;
+	float detectIn_a;	// Beginning detection angle for trajectories
+	float detectIn_b;
+	float transitionIn_a;	// Beginning transition angle for trajectories
+	float transitionIn_b;
+	float transitionOut_a;	// Midway transition point for trajectories, point at where the robot switches trajectories
+	float transitionOut_b;
+	float transitionEnd_a;	// Ending transition point
+	float transitionEnd_b;
 	// Rework specific values held, these are just placeholder for now
+	
+	Neighbor(Trajectory a, Trajectory b) {
+		trajID_a = a.getID();
+		trajID_b = b.getID();
+		
+		
+	}
 }
