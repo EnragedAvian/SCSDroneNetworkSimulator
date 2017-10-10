44756cf8c76ea934ce17222ddda9c31740eb81e6
CameronBossalini <cameron@bossalini.net>
2017-10-10 10:54:16 -0500

    Completed neighbor class
    Has variables for various parameters for a neighboring trajectory

---
+++ Neighbor.java
@@ -0,0 +1,14 @@
+// Neighbor class contains a links between the graph of trajectories
+
+public class Neighbor {
+	int hubTrajID;
+	int neighborTrajID;
+	float angle;	// Angle between the two trajectories measured from the positive x axis of the hub trajectory
+	float hubDetectIn;
+	float neighborDetectIn;
+	float hubTransitionIn;
+	float hubTransitionOut;
+	float neighborTransitionIn;
+	float neighborTransitionOut;
+	// Rework specific values held, these are just placeholder for now
+}
