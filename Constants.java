e3a6c03ac80bbb44ace6d0774a7e25215cb93f53
CameronBossalini <cameron@bossalini.net>
2017-10-10 10:53:42 -0500

    Created constants class
    Contains global constants, though may want to rework later on

---
+++ Constants.java
@@ -0,0 +1,7 @@
+// Constants class contains constant items that can be changed later (wifi range, traj radius, etc..)
+
+public class Constants {
+	public static float wifiRange = 10;
+	public static float trajRadius = 10;
+	public static float robotSpeed = (float)0.01;
+}
