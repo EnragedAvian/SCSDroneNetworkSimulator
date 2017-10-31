a3c9d6c864a4f3d015f0051297a6f5e2edd61c31
brianwei98 <brianwei98@gmail.com>
2017-10-31 12:21:44 -0500

    Fixed run loop

--- Driver.java
+++ Driver.java
@@ -2,26 +2,31 @@
 public class Driver {
 	public static void main(String args[]){
 		Window w = new Window();
-		if (Constants.running) {
-			run();
-			try {
-				Thread.sleep(5);
-			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-			w.repaint();
+		while(true){
+			//TODO time based loop
+			
+				run();
+				try {
+					Thread.sleep(5);
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				w.repaint();
+			
 		}
 		
 	}
 	
 	public static void run(){
-		for(Robot r: Robot.robots) {
-    		r.move();
-	    }
-	    
-	    for(Robot r: Robot.robots) {
-    		r.logic();
-	    }
+		if (Constants.running) {
+			for(Robot r: Robot.robots) {
+	    		r.move();
+		    }
+		    
+		    for(Robot r: Robot.robots) {
+	    		r.logic();
+		    }
+		}
 	}
 }
