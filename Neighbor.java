cd8d0c2abb8b51a492b0fded5d7eb4fae3b521c7
CameronBossalini <cameron@bossalini.net>
2017-10-31 00:37:16 -0500

    Merged work from Processing to Github tree

--- Neighbor.java
+++ Neighbor.java
@@ -3,96 +3,83 @@
 import java.lang.Math;
 
 public class Neighbor {
-	Trajectory traj_a;
-	Trajectory traj_b;
-	int trajID_a;	// IDs of the specific trajectories
-	int trajID_b;
-	int trajDir_a;	// Directions of the specific trajectories
-	int trajDir_b;
-	float trajDistance;	// Distance between the two trajectories
-	float angle_a;	// Angle between positive x axis and the angle of the trajectory
-	float angle_b;
-	float detectIn_a;	// Beginning detection angle for trajectories
-	float detectIn_b;
-	float transitionIn_a;	// Beginning transition angle for trajectories
-	float transitionIn_b;
-	float transitionOut_a;	// Ending point for transitions, opposite of the transitionin angle
-	float transitionOut_b;
-	// Rework specific values held, these are just placeholder for now
-	
-	Neighbor(Trajectory a, Trajectory b) {
-		traj_a = a;
-		traj_b = b;
-		
-		trajID_a = a.getID();
-		trajID_b = b.getID();
-		
-		trajDir_a = a.getDir();
-		trajDir_b = b.getDir();
-		
-		// Calculating the distance between the two trajectories
-		float xRange = Math.abs(a.getX() - b.getX());	// defining variables containing x and y distances between the trajectories
-		float yRange = Math.abs(a.getY() - b.getY());
-		float distance = (float)Math.sqrt((double)(xRange*xRange + yRange*yRange));
-		trajDistance = distance;
-		
-		// math for determining both angles a and b
-		if(a.getX()<=b.getX()) {	// Testing to see if a is to the left of b, ensuring that the angle_a falls between +/- pi/2
-			float xDist = b.getX()-a.getX();
-			float yDist = b.getY()-a.getY();
-			angle_a = (float)Math.atan((double)(yDist/xDist));
-			angle_b = (float)(angle_a+Math.PI);
-		} else {		// If a is to the right of b, use b as the reference point and measure angles from b
-			float xDist = a.getX()-b.getX();
-			float yDist = a.getY()-b.getY();
-			angle_b = (float)Math.atan((double)(yDist/xDist));
-			angle_a = (float)(angle_b+Math.PI);
-		}
-		// Placing angles within desired range
-		angle_a = normalizeAngle(angle_a);  
-		angle_b = normalizeAngle(angle_b);
+  Trajectory traj_a;
+  Trajectory traj_b;
+  int trajID_a;  // IDs of the specific trajectories
+  int trajID_b;
+  int trajDir_a;  // Directions of the specific trajectories
+  int trajDir_b;
+  float trajDistance;  // Distance between the two trajectories
+  float angle_a;  // Angle between positive x axis and the angle of the trajectory
+  float angle_b;
+  float detectIn_a;  // Beginning detection angle for trajectories
+  float detectIn_b;
+  float transitionIn_a;  // Beginning transition angle for trajectories
+  float transitionIn_b;
+  float transitionOut_a;  // Ending point for transitions, opposite of the transitionin angle
+  float transitionOut_b;
+  // Rework specific values held, these are just placeholder for now
+  
+  Neighbor(Trajectory a, Trajectory b) {
+    traj_a = a;
+    traj_b = b;
+    
+    trajID_a = a.getID();
+    trajID_b = b.getID();
+    
+    trajDir_a = a.getDir();
+    trajDir_b = b.getDir();
+    
+    // Calculating the distance between the two trajectories
+    float xRange = Math.abs(a.getX() - b.getX());  // defining variables containing x and y distances between the trajectories
+    float yRange = Math.abs(a.getY() - b.getY());
+    float distance = (float)Math.sqrt((double)(xRange*xRange + yRange*yRange));
+    trajDistance = distance;
+    
+    // math for determining both angles a and b
+    if(a.getX()<=b.getX()) {  // Testing to see if a is to the left of b, ensuring that the angle_a falls between +/- pi/2
+      float xDist = b.getX()-a.getX();
+      float yDist = b.getY()-a.getY();
+      angle_a = (float)Math.atan((double)(yDist/xDist));
+      angle_b = (float)(angle_a+Math.PI);
+    } else {    // If a is to the right of b, use b as the reference point and measure angles from b
+      float xDist = a.getX()-b.getX();
+      float yDist = a.getY()-b.getY();
+      angle_b = (float)Math.atan((double)(yDist/xDist));
+      angle_a = (float)(angle_b+Math.PI);
+    }
+    // Placing angles within desired range
+    angle_a = Constants.normalizeAngle(angle_a);  
+    angle_b = Constants.normalizeAngle(angle_b);
 
-		// math for determining the detectIn angle for both a and b
-		float baseDetect;		// Base angle, essentially calculating the angle between the detect range and angle_a/angle_b
-		baseDetect = (float)Math.acos((double)((distance - Constants.wifiRange)/Constants.trajRadius));
-		if (trajDir_a == 1) {
-			detectIn_a = angle_a - baseDetect;
-			detectIn_b = angle_b + baseDetect;
-		} else if (trajDir_a == -1) {
-			detectIn_a = angle_a + baseDetect;
-			detectIn_b = angle_b - baseDetect;
-		}
-		//placing angles within desired range
-		detectIn_a = normalizeAngle(detectIn_a);
-		detectIn_b = normalizeAngle(detectIn_b);  
-		
-		// math for determining the transitionIn and transitionOut angles for both trajectories.
-		float baseTransition;
-		baseTransition = (float)Math.acos((double)(2*Constants.trajRadius/distance));
-		if (trajDir_a == 1) {
-			transitionIn_a = angle_a - baseTransition;
-			transitionOut_a = angle_a + baseTransition;
-			transitionIn_b = angle_b + baseTransition;
-			transitionOut_b = angle_b - baseTransition;
-		} else if (trajDir_a == -1) {
-			transitionIn_a = angle_a + baseTransition;
-			transitionOut_a = angle_a - baseTransition;
-			transitionIn_b = angle_b - baseTransition;
-			transitionOut_b = angle_b + baseTransition;
-		}
-		
-	}
-	
-	static float normalizeAngle(float angle) {	// Function which places an angle between range of 0-2pi.
-		float newAngle = angle;
-		while(newAngle>2*Math.PI) {	// Making sure that angle_a is within the range of 0-2pi (decrementing)
-			newAngle -= (float)(2*Math.PI);
-		}
-		while(newAngle<2*Math.PI) {	// Making sure that angle_a is within the range of 0-2pi (incrementing)
-			newAngle += (float)(2*Math.PI);
-		}
-		return newAngle;
-	}
-	
-	
+    // math for determining the detectIn angle for both a and b
+    float baseDetect;    // Base angle, essentially calculating the angle between the detect range and angle_a/angle_b
+    baseDetect = (float)Math.acos((double)((distance - Constants.wifiRange)/(2*Constants.trajRadius)));
+    if (trajDir_a == 1) {
+      detectIn_a = angle_a - baseDetect;
+      detectIn_b = angle_b + baseDetect;
+    } else if (trajDir_a == -1) {
+      detectIn_a = angle_a + baseDetect;
+      detectIn_b = angle_b - baseDetect;
+    }
+    //placing angles within desired range
+    detectIn_a = Constants.normalizeAngle(detectIn_a);
+    detectIn_b = Constants.normalizeAngle(detectIn_b);  
+    
+    // math for determining the transitionIn and transitionOut angles for both trajectories.
+    float baseTransition;
+    baseTransition = (float)Math.acos((double)(2*Constants.trajRadius/distance));
+    if (trajDir_a == 1) {
+      transitionIn_a = Constants.normalizeAngle(angle_a - baseTransition);
+      transitionOut_a = Constants.normalizeAngle(angle_a + baseTransition);
+      transitionIn_b = Constants.normalizeAngle(angle_b + baseTransition);
+      transitionOut_b = Constants.normalizeAngle(angle_b - baseTransition);
+    } else if (trajDir_a == -1) {
+      transitionIn_a = Constants.normalizeAngle(angle_a + baseTransition);
+      transitionOut_a = Constants.normalizeAngle(angle_a - baseTransition);
+      transitionIn_b = Constants.normalizeAngle(angle_b - baseTransition);
+      transitionOut_b = Constants.normalizeAngle(angle_b + baseTransition);
+    }
+    
+  }
 }
--- Trajectory.java
+++ Trajectory.java
@@ -3,98 +3,110 @@
 import java.util.ArrayList;
 import java.lang.Math;
 
+
 public class Trajectory {
-	private float x;	// x coordinate of vertex
-	private float y;	// y coordinate of vertex
-	private int trajID;	// Identification number for trajectory
-	private int direction;
-	ArrayList<Neighbor> neighbors;	// Neighbors to be better defined later
-	private float radius = 10;
-	
-	
-	public ArrayList<Trajectory> trajectories = new ArrayList();
-	
-	/*public void buildNetwork() {
-		for (int i = 0; i<trajectories.size(); i++) {
-			
-		}
-	}*/
-	
-	public Trajectory(float xCoord, float yCoord, int id) {
-		x = xCoord;
-		y = yCoord;
-		//trajID = trajectories.size() + 1;
-		trajID = id;
-		
-		if (trajectories.size() == 0) {
-			direction = 1;
-		} else {
-			//Knowingly creating issues that can be fixed later. Namely, that there is no checking to see if trajectories are too close to one another
-			ArrayList<Trajectory> compareTrajectories = new ArrayList();
-			for (int i = 0; i<trajectories.size(); i++) {
-				if (trajectories.get(i).getID()!=trajID) {
-					if (inRange(trajectories.get(i))) {
-						compareTrajectories.add(trajectories.get(i));
-					}
-				}
-			}
-			int oldestTraj = 999999;	// Initializing to high value in order to find smaller values than it
-			int arrayID = 0;
-			for (int j = 0; j<compareTrajectories.size(); j++) {	// Creating link with the oldest trajectory in range first, then making other links based off of that.
-				 if (compareTrajectories.get(j).getID() < oldestTraj) {
-					 oldestTraj = compareTrajectories.get(j).getID();
-					 arrayID = j;
-				 }				
-			}
-			direction = -1 * compareTrajectories.get(arrayID).getDir();
-			for (int k = 0; k<compareTrajectories.size(); k++) {
-				if (direction == -1 * compareTrajectories.get(k).getDir()) {
-					addNeighbor(compareTrajectories.get(k));
-					compareTrajectories.get(k).addNeighbor(this);;
-				}
-			}
-		}
-		
-		trajectories.add(this);	// Adding trajectory to the arraylist of trajectories
-	}
-	
-	int getID() {
-		return trajID;
-	}
-	
-	float getX() {
-		return x;
-	}
-	
-	float getY() {
-		return y;
-	}
-	
-	void setX(float newX) {
-		x = newX;
-	}
-	
-	void setY(float newY) {
-		y = newY;
-	}
-	
-	int getDir() {
-		return direction;
-	}
-	
-	void addNeighbor(Trajectory t) {
-		Neighbor newNeighbor = new Neighbor(this, t);
-		neighbors.add(newNeighbor);
-	}
-	
-	boolean inRange(Trajectory t) {
-		float xDist = Math.abs(x - t.getX());
-		float yDist = Math.abs(y - t.getY());
-		float distance = (float)Math.sqrt((double)(xDist*xDist + yDist*yDist));
-		if (distance <= (float)(2*radius/Math.cos(Math.PI/12))) {
-			return true;
-		} else {
-			return false;
-		}
-	}
+  private float x;  // x coordinate of vertex
+  private float y;  // y coordinate of vertex
+  private int trajID;  // Identification number for trajectory
+  private int direction;
+  ArrayList<Neighbor> neighbors;  // Neighbors to be better defined later
+  
+  
+  public static ArrayList<Trajectory> trajectories = new ArrayList<Trajectory>();
+  
+  /*public void buildNetwork() {
+    for (int i = 0; i<trajectories.size(); i++) {
+      
+    }
+  }*/
+  
+  public Trajectory(float xCoord, float yCoord) {
+    x = xCoord;
+    y = yCoord;
+    
+    trajectories.add(this);  // Adding trajectory to the arraylist of trajectories
+    
+    trajID = trajectories.size();
+    System.out.println("Trajectory size: " + trajectories.size());
+    
+    neighbors = new ArrayList<Neighbor>();
+    
+    
+    
+    if (trajectories.size() == 1) {
+      direction = 1;
+      System.out.println("Created Direction for trajectory " + trajID);
+    } else {
+      //Knowingly creating issues that can be fixed later. Namely, that there is no checking to see if trajectories are too close to one another
+      ArrayList<Trajectory> compareTrajectories = new ArrayList<Trajectory>();
+      
+      
+      for (int i = 0; i<trajectories.size(); i++) {
+        if (trajectories.get(i).getID()!=trajID) {
+          if (inRange(trajectories.get(i))) {
+            compareTrajectories.add(trajectories.get(i));
+            System.out.println("Trajectory found within range!");
+          }
+        }
+      }
+      int oldestTraj = 999999;  // Initializing to high value in order to find smaller values than it
+      int arrayID = 0;
+      for (int j = 0; j<compareTrajectories.size(); j++) {  // Creating link with the oldest trajectory in range first, then making other links based off of that.
+         if (compareTrajectories.get(j).getID() < oldestTraj) {
+           oldestTraj = compareTrajectories.get(j).getID();
+           arrayID = j;
+         }        
+      }
+      if(compareTrajectories.size() > 0) {
+        direction = -1 * compareTrajectories.get(arrayID).getDir();
+        for (int k = 0; k<compareTrajectories.size(); k++) {
+          if (direction != compareTrajectories.get(k).getDir()) {
+            addNeighbor(compareTrajectories.get(k));
+            compareTrajectories.get(k).addNeighbor(this);
+            System.out.println("Added a neighbor!");
+            System.out.println("Neighbor added: " + neighbors.get(0).trajID_b);
+          }
+        }
+      } else {
+        direction = 1;
+      }
+      
+      
+    }
+  }
+  
+  int getID() {
+    return trajID;
+  }
+  
+  float getX() {
+    return x;
+  }
+  
+  float getY() {
+    return y;
+  }
+  
+  int getDir() {
+    return direction;
+  }
+  
+  void addNeighbor(Trajectory t) {
+    Neighbor newNeighbor = new Neighbor(this, t);
+    neighbors.add(newNeighbor);
+  }
+  
+  boolean inRange(Trajectory t) {
+    float xDist = Math.abs(x - t.getX());
+    float yDist = Math.abs(y - t.getY());
+    System.out.println("xDist: " + xDist + " yDist: " + yDist);
+    float distance = (float)Math.sqrt((double)(xDist*xDist + yDist*yDist));
+    System.out.println("distance: " + distance);
+    System.out.println("Maximum distance: " + (2*Constants.trajRadius/Math.cos(Math.PI/12)));
+    if (distance <= (float)(2*Constants.trajRadius/Math.cos(Math.PI/6))) {
+      return true;
+    } else {
+      return false;
+    }
+  }
 }
